1d9c2d7adfec3bd9cdcdd0de2e15f0783685f167
Rafael Laurindo <rafaelplaurindo@gmail.com>
2018-08-09 16:55:52 -0300

    removes comments

--- projects/ngen-restful-crud/src/public_api.ts
+++ projects/ngen-restful-crud/src/public_api.ts
@@ -1,18 +1,2 @@
 import * as NGenRESTfulCRUD from './lib';
 export { NGenRESTfulCRUD };
-
-// class Model implements NGenRESTfulCRUD.GenericAbstractProduct<Model> {
-
-//     getConcrete(): Model {
-//         return this;
-//     }
-
-// }
-
-// class Test extends NGenRESTfulCRUD.GenericAbstractService<Model> {
-
-//     manufacture(response: Response): NGenRESTfulCRUD.GenericAbstractProduct<Model> {
-//         return new Model();
-//     }
-
-// }
