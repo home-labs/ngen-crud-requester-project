16f1e6a15fc8bbbcfb2fda515d3f1c4d68bea014
Rafael Laurindo <rafaelplaurindo@gmail.com>
2019-02-21 22:31:55 -0300

    adequations

--- projects/ngen-restful-crud/src/lib/index.ts
+++ projects/ngen-restful-crud/src/lib/index.ts
@@ -1,5 +1,5 @@
 export { GeneralService } from './general-service';
-
+export { GenericAbstractService } from './generic-abstract-service';
 export { GenericAbstractProduct } from './generic-abstract-product';
 
 export { NGenRESTfulCRUDModule } from './module';
