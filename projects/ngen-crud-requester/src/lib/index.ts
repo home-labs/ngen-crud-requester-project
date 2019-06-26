f8cfbade12651e1b49b2a75d22c334b5ba7867c2
Rafael Laurindo <rafaelplaurindo@gmail.com>
2019-06-26 16:20:27 -0300

    adequations

--- projects/ngen-crud-requester/src/lib/index.ts
+++ projects/ngen-crud-requester/src/lib/index.ts
@@ -1,4 +1,2 @@
 export { GeneralService } from './general-service';
 export { Strategies } from './strategies/namespace';
-// export { GenericAbstractService } from './generic-abstract-service';
-// export { GenericAbstractProduct } from './generic-abstract-product';
