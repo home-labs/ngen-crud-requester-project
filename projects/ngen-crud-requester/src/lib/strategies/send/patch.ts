0de659916a43514738d20113fa2e2f2f89b3d924
Rafael Pereira Laurindo <rplaurindo>
2019-09-03 19:11:13 -0300

    adequations

--- projects/ngen-crud-requester/src/lib/strategies/send/patch.ts
+++ projects/ngen-crud-requester/src/lib/strategies/send/patch.ts
@@ -4,7 +4,7 @@ import { AbstractSend } from './abstract-send';
 export class Patch extends AbstractSend {
 
     send(url: string, data: object, options?: object): Promise<Response> {
-        return super.send(url, data, options, 'patch') as Promise<Response>;
+        return super.send(url, data, options, 'patch');
     }
 
 }
